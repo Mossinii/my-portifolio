6124f1107376c50545c8a9b4de09b81115210d94
Mossinii <117039278+Mossinii@users.noreply.github.com>
2023-07-03 05:54:05 -0300

    feat: portfólio changes

---
+++ src/app/components/translate-btn/translate-btn.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { BsFillFlagFill } from 'react-icons/bs';
+import { FiFlag } from 'react-icons/fi';
+
+"use client"
+
+const TranslateBtn = () => {
+  const [language, setLanguage] = useState('pt-BR');
+
+  const handleLanguageChange = () => {
+    const newLanguage = language === 'pt-BR' ? 'en' : 'pt-BR';
+    setLanguage(newLanguage);
+  };
+
+  return (
+    <div className="translate-btn">
+      {language === 'pt-BR' ? (
+        <BsFillFlagFill
+          onClick={handleLanguageChange}
+          className={`flag-icon ${language === 'pt-BR' ? 'active' : ''}`}
+        />
+      ) : (
+        <FiFlag
+          onClick={handleLanguageChange}
+          className={`flag-icon ${language === 'en' ? 'active' : ''}`}
+        />
+      )}
+    </div>
+  );
+};
+
+export default TranslateBtn;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { Header } from './components/header/header'
 import { EmailIcon } from './components/icons/email.icon'
 import { Info } from './components/information/information'
 import { SocialBtns } from './components/social-btns/social-btns'
+import TranslateBtn from './components/translate-btn/translate-btn'
 
 import './styles/home.scss'
 
 export default function Home() {
   return (
     <main className="container">
+      <TranslateBtn/> 
     <Header/>
     <Experience/>
     <Info/>
