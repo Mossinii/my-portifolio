a67e2877e0f59340d2ed78ff20f8203e14b0d500
Mossinii <117039278+Mossinii@users.noreply.github.com>
2023-10-01 16:41:56 -0300

    teste 2

--- src/app/components/translate-btn/translate-btn.tsx
+++ src/app/components/translate-btn/translate-btn.tsx
@@ -9,6 +9,15 @@ import { FiFlag } from 'react-icons/fi';
 const TranslateBtn = () => {
   const [language, setLanguage] = useState('pt-BR');
 
+  const handleLanguageChange = () => {
+    const newLanguage = language === 'pt-BR' ? 'en' : 'pt-BR';
+    setLanguage(newLanguage);
+  };
+
+  return (
+    <div className="translate-btn">
+    </div>
+  );
 };
 
 export default TranslateBtn;
